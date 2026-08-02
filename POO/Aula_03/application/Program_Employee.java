8d32c8e90043e1d4644d11aa5e66a018d63e457f
vitinho455 <victorhugonunessilva455@gmail.com>
2026-08-02 19:10:42 -0300

    Refactor: Melhora na identificação do ID.

--- POO/Aula_03/application/Program_Employee.java
+++ POO/Aula_03/application/Program_Employee.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Employee;
@@ -11,12 +12,13 @@ public class Program_Employee {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
 		List<Employee> list_employee = new ArrayList<>();
 	
-		System.out.println("How many employees will be registered? ");
+		System.out.print("How many employees will be registered? ");
 		int x = sc.nextInt();
 		
 		for (int i = 0; i < x; i++) {
@@ -41,8 +43,10 @@ public class Program_Employee {
 			
 		}
 		
-		System.out.println("Enter the employee id that will have salary increase :");
+		System.out.print("Enter the employee id that will have salary increase :");
 		int search_id = sc.nextInt();
+		
+		boolean found = false;
 		
 		for (Employee employee : list_employee) {
 			
@@ -50,15 +54,16 @@ public class Program_Employee {
 				
 				System.out.println("Enter the percentage: ");
 				double percentage = sc.nextDouble();
-				employee.increaseSalary(percentage);
-				
-			}else {
-				
-				System.out.println("This id does not exist!");			
-				System.out.println();
-				break;
-			}
+				employee.increaseSalary(percentage);				
+				found = true;
+				break;			
+			} 
 			
+		}
+		
+		if(!found) {
+			
+			System.out.println("This id does not exist!");
 		}
 				
 		for (Employee employee : list_employee) {
--- POO/Aula_03/entities/Employee.java
+++ POO/Aula_03/entities/Employee.java
@@ -51,6 +51,6 @@ public class Employee {
 				+ ": " 
 				+ name 
 				+ ", " 
-				+ salary;
+				+ String.format("%.2f",salary);
 	}
 }
